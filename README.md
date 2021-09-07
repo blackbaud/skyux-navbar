4e2d021efa09a03daf9539ccd6f3301de35dd034
Trevor Burch <Trevor.Burch@blackbaud.com>
2021-09-07 16:11:35 -0400

    Next > Adopt Angular CLI

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# @skyux/navbar
+# @skyux/navbar Workspace
 
-[![npm](https://img.shields.io/npm/v/@skyux/navbar.svg)](https://www.npmjs.com/package/@skyux/navbar)
-![SKY UX CI](https://github.com/blackbaud/skyux-navbar/workflows/SKY%20UX%20CI/badge.svg)
-[![coverage](https://codecov.io/gh/blackbaud/skyux-navbar/branch/master/graphs/badge.svg?branch=master)](https://codecov.io/gh/blackbaud/skyux-navbar/branch/master)
+For information about `@skyux/navbar` please visit the library's README file, located at `projects/navbar/README.md`.
---
+++ projects/navbar-showcase/src/app/visual/visual.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VisualComponent } from './visual.component';
+import { SkyNavbarModule } from 'projects/navbar/src/public-api';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SkyDropdownModule } from '@skyux/popovers';
+import { NavbarVisualComponent } from './navbar/navbar-visual.component';
+
+@NgModule({
+  declarations: [
+    VisualComponent,
+    NavbarVisualComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    SkyNavbarModule,
+    SkyDropdownModule
+  ]
+})
+export class VisualModule { }
