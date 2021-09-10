52cd7ab036f1bc85411824048f01bba246bb2cd7
Trevor Burch <Trevor.Burch@blackbaud.com>
2021-09-10 11:25:54 -0400

    Next > Adopt Angular CLI (#20)
    * Next > Adopt Angular CLI
    
    * Commit miss
    
    * Fix merge issue
    
    * Update dependencies
    
    * Review changes
    
    * Update dependencies

---
+++ projects/navbar-showcase/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarVisualComponent } from './visual/navbar/navbar-visual.component';
+import { VisualComponent } from './visual/visual.component';
+
+const routes: Routes = [{
+  path: '',
+  component: VisualComponent
+},
+{
+  path: 'visual/navbar',
+  component: NavbarVisualComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
